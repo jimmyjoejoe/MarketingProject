7dfd943e4e7fe8eddf0d5cad6d3d09754ba2fa2f
jimmyjoejoe <NeilJ9530@sduhsd.lan>
2016-05-13 09:11:53 -0700

    Update 1.1
    -Added numerous scripts
    -Fixed ball bounciness
    -Scripts don't work properly

---
+++ Marketing Game Project - Brick Bracker/Assets/Scripts/Bricks.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bricks : MonoBehaviour {
+
+	public GameObject game;
+	
+	void OnCollision2DEnter (Collision2D other)
+	{
+		GM.instance.DestroyBrick();
+		Destroy(game);
+	}
+}
--- Marketing Game Project - Brick Bracker/Assets/Scripts/HitbrickScript.cs
+++ Marketing Game Project - Brick Bracker/Assets/Scripts/HitbrickScript.cs
@@ -3,4 +3,10 @@ using System.Collections;
 
 public class HitbrickScript : MonoBehaviour {
 	public bool hitbrick;
+	
+	void OnCollision2DEnter (Collision2D other) {
+
+		hitbrick = true;
+
+	}
 }
