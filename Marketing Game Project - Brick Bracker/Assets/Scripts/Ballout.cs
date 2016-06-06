0287eac009e60f01dba0dd99c55f089647f71c92
slashers08 <jeffreyt101@gmail.com>
2016-06-06 09:25:01 -0700

    Upload J

---
+++ Marketing Game Project - Brick Bracker/Assets/Scripts/Ballout.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Ballout : MonoBehaviour {
+
+	public int lives;
+	public GameObject Ball;
+	public Text LivesText;
+
+	void Start (){
+		lives = 3;
+		Livescount ();
+	}
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if (other.tag == "Ball") {
+			lives = lives - 1;
+			Livescount ();
+		}
+
+	}
+	void Livescount (){
+		LivesText.text = "lives: " + lives.ToString ();
+}
+}
