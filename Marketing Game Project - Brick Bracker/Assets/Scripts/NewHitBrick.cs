87a5a83f774f6919c6f58b3560ab62714fbe1038
patrice0nguyen <NguyenP7123@sduhsd.lan>
2016-06-06 09:26:39 -0700

    Changed BrickHit Scripts
    - animation no longer works
    - not all bricks have been set

---
+++ Marketing Game Project - Brick Bracker/Assets/Scripts/NewHitBrick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewHitBrick : MonoBehaviour {
+
+	private Animator BrickAnim;
+	public GameObject Red;
+	public GameObject Orange;
+	public GameObject Yellow;
+	public GameObject Green;
+	public GameObject Blue;
+	public GameObject Purple;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+
+	void OnCollisionEnter2D (Collision2D other){
+		if (other.gameObject.tag == "Purple") {
+			Destroy (other.gameObject);
+			Instantiate (Blue,transform.position,transform.rotation);
+					}
+
+		if (other.gameObject.tag == "Red") {
+			BrickAnim = other.gameObject.GetComponent <Animator> ();
+			BrickAnim.SetBool ("Brick",true);
+			Destroy (other.gameObject);		
+		}
+
+	}
+}
